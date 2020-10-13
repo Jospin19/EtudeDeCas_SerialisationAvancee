77dd61bd44f03d2bddb62b2fcaf0566c7d24b18b
Jospin19 <nssoekeme@gmail.com>
2020-10-13 10:32:13 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EtudeDeCas_SerialisationAvancee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
